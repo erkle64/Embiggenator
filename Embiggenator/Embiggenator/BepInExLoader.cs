6a7e20b94b7101f73e2fb40ef9dae93322d4491e
erkle64 <erkle64@gmail.com>
2022-05-31 04:43:00 +1200

    Initial source code commit

---
+++ Embiggenator/Embiggenator/BepInExLoader.cs
@@ -0,0 +1,61 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using UnhollowerRuntimeLib;
+using HarmonyLib;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Embiggenator
+{
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public class BepInExLoader : BepInEx.IL2CPP.BasePlugin
+    {
+        public const string
+            MODNAME = "Embiggenator",
+            AUTHOR = "erkle64",
+            GUID = "com." + AUTHOR + "." + MODNAME,
+            VERSION = "1.0.0";
+
+        public static BepInEx.Logging.ManualLogSource log;
+
+        public BepInExLoader()
+        {
+            log = Log;
+        }
+
+        public override void Load()
+        {
+            log.LogMessage("Registering PluginComponent in Il2Cpp");
+
+            var config_additionalInventorySlots = Config.Bind("Inventory", "additionalSlots", 32, "Number of slots to add to character inventory space.");
+            PluginComponent.additionalInventorySlots = config_additionalInventorySlots.Value;
+            if (PluginComponent.additionalInventorySlots < 0) PluginComponent.additionalInventorySlots = 0;
+
+            try
+            {
+                ClassInjector.RegisterTypeInIl2Cpp<PluginComponent>();
+
+                var go = new GameObject("Erkle64_Embiggenator_PluginObject");
+                go.AddComponent<PluginComponent>();
+                Object.DontDestroyOnLoad(go);
+            }
+            catch
+            {
+                log.LogError("FAILED to Register Il2Cpp Type: PluginComponent!");
+            }
+
+            try
+            {
+                var harmony = new Harmony(GUID);
+
+                var original = AccessTools.Method(typeof(CharacterManager), "Init");
+                var post = AccessTools.Method(typeof(PluginComponent), "Init");
+                harmony.Patch(original, postfix: new HarmonyMethod(post));
+            }
+            catch
+            {
+                log.LogError("Harmony - FAILED to Apply Patch's!");
+            }
+        }
+    }
+}
---
+++ Embiggenator/Embiggenator/PluginComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnhollowerBaseLib;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Embiggenator
+{
+    public class PluginComponent : MonoBehaviour
+    {
+        public static int additionalInventorySlots = 0;
+
+        public PluginComponent (IntPtr ptr) : base(ptr)
+        {
+        }
+
+        [HarmonyPostfix]
+        public static void Init(CubeSavegame savegame, bool isServerJoin)
+        {
+            if (additionalInventorySlots > 0)
+            {
+                BepInExLoader.log.LogMessage(string.Format("Embiggenating inventory by {0} slots", additionalInventorySlots));
+                CharacterManager.increasePlayerInventorySizeByResearch(additionalInventorySlots);
+            }
+        }
+    }
+}
