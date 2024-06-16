c9127dc923e00a862b0c7b1c501205ecd22dd7c5
erkle64 <erkle64@gmail.com>
2024-06-17 07:32:25 +1200

    Added scrollbar to inventory frame.

---
+++ Embiggenator/AdaptablePreferred.cs
@@ -0,0 +1,43 @@
+﻿using Embiggenator;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdaptablePreferred : LayoutElement, ILayoutElement
+{
+    [SerializeField] private RectTransform _contentToGrowWith;
+
+    [SerializeField] private bool _usePreferredHeight;
+    [SerializeField] private float _preferredHeightMax;
+
+    private float _preferredHeight;
+
+    public override float preferredHeight => _preferredHeight;
+
+    public void Setup(RectTransform contentToGrowWith, bool usePreferredHeight, float preferredHeightMax)
+    {
+        _contentToGrowWith = contentToGrowWith;
+        _usePreferredHeight = usePreferredHeight;
+        _preferredHeightMax = preferredHeightMax;
+    }
+
+    public override void CalculateLayoutInputVertical()
+    {
+        if (_contentToGrowWith == null)
+        {
+            return;
+        }
+        if (_usePreferredHeight)
+        {
+            var height = LayoutUtility.GetPreferredHeight(_contentToGrowWith);
+            _preferredHeight = _preferredHeightMax > height ? height : _preferredHeightMax;
+        }
+        else
+        {
+            _preferredHeight = -1;
+        }
+    }
+
+    public override void CalculateLayoutInputHorizontal()
+    {
+    }
+}
--- Embiggenator/Plugin.cs
+++ Embiggenator/Plugin.cs
@@ -1,21 +1,25 @@
 using C3.ModKit;
 using HarmonyLib;
 using Unfoundry;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Embiggenator
 {
-    [UnfoundryMod(Plugin.GUID)]
+    [UnfoundryMod(GUID)]
     public class Plugin : UnfoundryPlugin
     {
         public const string
             MODNAME = "Embiggenator",
             AUTHOR = "erkle64",
             GUID = AUTHOR + "." + MODNAME,
-            VERSION = "1.3.0";
+            VERSION = "1.4.0";
 
         public static LogSource log;
 
         public static TypedConfigEntry<int> inventorySlots;
+        private static TypedConfigEntry<int> maxVerticalSize;
+        private static TypedConfigEntry<int> scrollSpeed;
         public static TypedConfigEntry<bool> disableInventoryResearch;
 
         public Plugin()
@@ -23,7 +27,9 @@ namespace Embiggenator
             log = new LogSource(MODNAME);
             new Config(GUID)
                 .Group("Inventory")
-                    .Entry(out inventorySlots, "Slot Count", 70, "Total number of inventory slots players should have.")
+                    .Entry(out inventorySlots, "Slot Count", 70, "Total number of inventory slots players should have.", "Maximum size: 200")
+                    .Entry(out maxVerticalSize, "Max Vertical Size", 8, "Maximum number rows to display before adding a scrollbar.")
+                    .Entry(out scrollSpeed, "Scroll Speed", 40, "Speed to scroll the inventory window when using the mouse wheel.")
                 .EndGroup()
                 .Group("Research")
                     .Entry(out disableInventoryResearch, "Disable Inventory Research", true, "Prevents research from increasing inventory size.")
@@ -44,10 +50,11 @@ namespace Embiggenator
             [HarmonyPostfix]
             public static void joinWorld(Character character, uint clientId)
             {
-                if (inventorySlots.Get() > 0)
+                var inventorySlots = Mathf.Clamp(Plugin.inventorySlots.Get(), 10, 200);
+                if (inventorySlots > 0)
                 {
                     var currentInventorySlots = InventoryManager.inventoryManager_getInventorySlotCountByPtr(character.inventoryPtr);
-                    var additionalInventorySlots = inventorySlots.Get() - currentInventorySlots;
+                    var additionalInventorySlots = inventorySlots - currentInventorySlots;
                     if (additionalInventorySlots > 0)
                     {
                         log.Log($"Embiggenating inventory by {additionalInventorySlots} slots for {character.username}");
@@ -58,6 +65,44 @@ namespace Embiggenator
                         log.Log($"Skipping {character.username}, they already have {currentInventorySlots} slots");
                     }
                 }
+            }
+
+            [HarmonyPatch(typeof(CharacterInventoryFrame), nameof(CharacterInventoryFrame.Init))]
+            [HarmonyPostfix]
+            public static void CharacterInventoryFrame_Init(CharacterInventoryFrame __instance)
+            {
+                if (__instance.transform.Find("ScrollBox")) return;
+
+                log.Log("Creating scrollbox for character inventory frame.");
+
+                UIBuilder.BeginWith(__instance.itemSlotContainer.transform.parent.gameObject)
+                    .Element_ScrollBox("ScrollBox", contentBuilder =>
+                    {
+                        __instance.itemSlotContainer.transform.SetParent(contentBuilder.GameObject.transform.parent, false);
+                        Object.DestroyImmediate(contentBuilder.GameObject);
+                    })
+                        .WithComponent<ScrollRect>(scrollBox =>
+                        {
+                            scrollBox.horizontal = false;
+                            scrollBox.content = __instance.itemSlotContainer.GetComponent<RectTransform>();
+                            scrollBox.movementType = ScrollRect.MovementType.Clamped;
+                            scrollBox.scrollSensitivity = scrollSpeed.Get();
+                        })
+                        .With(scrollBox =>
+                        {
+                            var grid = __instance.itemSlotContainer.GetComponent<GridLayoutGroup>();
+                            var maxSize = grid.cellSize.y * maxVerticalSize.Get() + grid.spacing.y * (maxVerticalSize.Get() - 1);
+                            scrollBox.AddComponent<AdaptablePreferred>()
+                                .Setup(__instance.itemSlotContainer.GetComponent<RectTransform>(), true, maxSize);
+                            scrollBox.transform.SetAsFirstSibling();
+                        })
+                    .Done
+                    .End();
+
+                UIBuilder.BeginWith(__instance.itemSlotContainer.gameObject)
+                    .SetRectTransform(0, 0, 0, 0, 0, 1, 0, 1, 1, 1)
+                    .AutoSize(ContentSizeFitter.FitMode.Unconstrained, ContentSizeFitter.FitMode.PreferredSize)
+                    .End();
             }
 
             [HarmonyPatch(typeof(CharacterManager), nameof(CharacterManager.increasePlayerInventorySizeByResearch))]
